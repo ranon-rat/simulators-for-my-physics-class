f0a312197cdd53f8b71b98dcb972b9824afcd38b
FlamesX-128 <glowingorangeflames@gmail.com>
2022-02-14 22:37:53 -0600

    estilos cambiados

--- build.bat
+++ build.bat
@@ -1,6 +1,17 @@
-#solo debes de ejecutar este archivo
-# es demasiado sencillo , dale click
+# solo debes de ejecutar este archivo
+# es demasiado sencillo, dale 
+
 tsc --build tsconfig.json
+
+cd public/
+sass styles
+
+for i in "mcu-simulator"; do
+  cd $i
+  sass styles
+  cd ..
+done
+
+cd ..
+
 go build main.go
-
-
--- main.go
+++ main.go
@@ -6,9 +6,8 @@ import (
 )
 
 func main() {
-	//serve static content
-	http.Handle("/", http.FileServer(http.Dir(".")))
-	log.Println("ve a la url http://localhost:8080/")
+	http.Handle("/", http.FileServer(http.Dir("public/")))
 
+	log.Println("ve a la url http://localhost:8080/")
 	http.ListenAndServe(":8080", nil)
 }
