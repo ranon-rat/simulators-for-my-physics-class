d2206453541a7358e3c9caa40878172486a8aa39
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2022-02-16 13:00:06 -0600

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,17 +1,10 @@
 # solo debes de ejecutar este archivo
 # es demasiado sencillo, dale 
-
+# click
 tsc --build tsconfig.json
 
-cd public/
-sass styles
 
-for i in "mcu-simulator"; do
-  cd $i
-  sass styles
-  cd ..
-done
 
-cd ..
+sass ./.
 
 go build main.go
