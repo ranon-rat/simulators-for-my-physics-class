eed7d80a98ae7c02d2c73c34f95af6457fed4731
FlamesX-128 <glowingorangeflames@gmail.com>
2022-02-22 23:47:51 -0600

    Mejoras en el simulador planetary-orbit

--- public/planetary-orbit-simulator/scripts/main.ts
+++ public/planetary-orbit-simulator/scripts/main.ts
@@ -1,57 +1,75 @@
-const canvas = document.getElementById("screen") as HTMLCanvasElement;
-const ctx = canvas.getContext("2d")!;
+const canvas = document.getElementById("screen") as HTMLCanvasElement,
+  ctx = canvas.getContext("2d")!;
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 ctx.strokeStyle = "#fff";
 
-// Draw the ellipse
-ctx.beginPath();
-ctx.ellipse(
-  window.innerWidth / 2,
-  window.innerHeight / 2,
-  window.innerWidth / 4,
-  window.innerHeight / 5,
-  0,
-  0,
-  2 * Math.PI
-);
-ctx.stroke();
+class Orbit {
+  // Esto equivale a la posicion 0 de "x" y "y" de una recta.
+  private posX: number = window.innerWidth / 2;
+  private posY: number = window.innerHeight / 2;
 
-ctx.beginPath();
-ctx.ellipse(
-  window.innerWidth / 2,
-  window.innerHeight / 2,
-  window.innerWidth / 3 - 20,
-  window.innerHeight / 4,
-  0,
-  0,
-  2 * Math.PI
-);
-ctx.stroke();
+  // Esto es para escalar los tamaños
+  public scale: number = 30;
 
+  // Esto son las variables para trazar el elipse.
+  public radiusA = 25;
+  public radiusB = 16;
+  public rotation = 0;
 
-// Draw the ellipse's line of reflection
-ctx.beginPath();
-ctx.setLineDash([5, 5]);
-ctx.moveTo(window.innerWidth, window.innerHeight / 2);
-ctx.lineTo(0, window.innerHeight / 2);
-ctx.stroke();
+  public readonly ctx: CanvasRenderingContext2D;
 
-// Draw the ellipse's line of reflection
-ctx.beginPath();
-ctx.setLineDash([5, 5]);
-ctx.moveTo(window.innerWidth / 2, window.innerHeight);
-ctx.lineTo(window.innerWidth / 2, 0);
-ctx.stroke();
+  constructor(ctx: CanvasRenderingContext2D) {
+    this.ctx = ctx;
 
+    this.ellipse();
+    this.lines_of_reflection();
+    this.red_sun();
+  }
 
-ctx.shadowBlur = 6;
-ctx.shadowColor = "#FA2705"
+  private lines_of_reflection() {
+    // Draw the ellipse's line of reflection
+    ctx.beginPath();
+    ctx.setLineDash([5, 5]);
+    ctx.moveTo(window.innerWidth, window.innerHeight / 2);
+    ctx.lineTo(0, window.innerHeight / 2);
+    ctx.stroke();
 
-// Sun
-ctx.beginPath();
-ctx.arc((window.innerWidth / 3) + 40, window.innerHeight / 2, 25, 0, 2 * Math.PI);
-ctx.fillStyle = "#FA2705"
-ctx.fill()
+    // Draw the ellipse's line of reflection
+    ctx.beginPath();
+    ctx.setLineDash([5, 5]);
+    ctx.moveTo(window.innerWidth / 2, window.innerHeight);
+    ctx.lineTo(window.innerWidth / 2, 0);
+    ctx.stroke();
+  }
+
+  public ellipse() {
+    ctx.beginPath();
+    ctx.ellipse(
+      this.posX,
+      this.posY,
+      Math.sqrt(this.radiusA) * this.scale,
+      Math.sqrt(this.radiusB) * this.scale,
+      this.rotation,
+      0,
+      2 * Math.PI
+    );
+    ctx.stroke();
+  }
+
+  private red_sun() {
+    ctx.shadowBlur = 6;
+    ctx.shadowColor = "#FA2705";
+
+    const x = Math.sqrt(this.radiusA - this.radiusB) * this.scale;
+
+    ctx.beginPath();
+    ctx.arc(this.posX - x, this.posY, this.scale / 2, 0, 2 * Math.PI);
+    ctx.fillStyle = "#FA2705";
+    ctx.fill();
+  }
+}
+
+const orbit = new Orbit(ctx);
