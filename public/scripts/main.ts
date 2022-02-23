abe08fa02ab123f2082646a0565da8c169d96082
FlamesX-128 <glowingorangeflames@gmail.com>
2022-02-22 21:25:45 -0600

    Estilos del inicio cambiados y renderizado de rutas

--- public/scripts/main.ts
+++ public/scripts/main.ts
@@ -1,7 +1,56 @@
-(function main() {
-  const elements = document.getElementsByClassName("fadeUp");
+const routes = [
+  [
+    "mcu-simulator",
+    "Movimiento Circular Uniforme",
+    "Es un simulador simple para observar y manipular el fenómeno del movimiento circular uniforme.",
+  ],
+  [
+    "planetary-orbit-simulator",
+    "Órbita Planetaria",
+    "Es un simulador simple para poder observar y manipular la orbita de los planetas.",
+  ],
+];
 
-  for (let i = 0, j = [2, 4, 6, 8]; i < elements.length; i++) // @ts-ignore
-    setTimeout(() => (elements[i].style["animation-play-state"] = "running"), +(j[i] + "000"))
+await (async function main() {
+  const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
+
+  for (const route of routes)
+    document.getElementById("route-list")!.innerHTML += `
+      <div class="route-list-container animated animatedFadeInUp fadeInUp">
+        <a href="#/${route[0]}/">
+          <div class="route-content">
+            <div class="route-content-title">
+              <h2>${route[1]}</h2>
+              <hr>
+            </div>
+
+            <div class="route-content-desc">
+              <p>${route[2]}</p>
+            </div>
+          </div>
+        </a>
+      </div>
+    `;
+
+  for (const animation of document.getElementsByClassName("fadeInUp")) {
+    await sleep(1000);
+
+    // @ts-ignore
+    animation.style["animation-play-state"] = "running"
+  }
+
+  await sleep(1000);
+
+  const document_viewer = document.getElementById("document-viewer")!;
+
+  window.location.hash = "#/";
+  window.location.hash = "#/mcu-simulator/"
+
+  window.addEventListener("hashchange", () => {
+    console.log(window.location.hash); // @ts-ignore
+    document_viewer.src = window.location.hash.slice(2);
+  });
 
 })();
+
+export {};
